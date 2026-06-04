90db0c3a2b846ecd99b25a6568e400a3130d572d
Awais Jamil <awais@myocchealthrecord.com>
2026-06-05 09:59:55 +1000

    Initial Commit

---
+++ Salah Logbook/Salah_LogbookApp.swift
@@ -0,0 +1,32 @@
+//
+//  Salah_LogbookApp.swift
+//  Salah Logbook
+//
+//  Created by mohr on 5/6/2026.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct Salah_LogbookApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
